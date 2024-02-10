e448889cd9d0f124d712409811fb462f520fc25b
Nikola Hristov <nikola@nikolahristov.tech>
2024-02-10 10:25:44 +0200

    squash!

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -292,7 +292,7 @@ is-at-least 2.30 "$git_version" \
   || alias gpsupf='git push --set-upstream origin $(git_current_branch) --force-with-lease'
 alias gpv='git push --verbose'
 alias gpoat='git push origin --all && git push origin --tags'
-alias gpod='git push origin --delete'
+alias gpod='git push --delete origin'
 alias ggpush='git push origin "$(git_current_branch)"'
 
 function ggp() {
